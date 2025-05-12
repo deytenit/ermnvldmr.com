ce013e118379a8ad2657fc2ef2b301618a0c44d7
Vladimir Eremin <personal@ermnvldmr.com>
2025-05-12 20:27:09 +0300

    fix: Typo in index.astro

--- release.config.mjs
+++ release.config.mjs
@@ -1,4 +1,8 @@
 export default {
-  branches: ["main", { name: "next", prerelease: true }],
-  plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/github']
+  branches: ['main', { name: 'next', prerelease: true }],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+  ],
 };
