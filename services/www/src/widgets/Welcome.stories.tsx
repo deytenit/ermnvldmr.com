a12b03b3b571c917713b4eacaaf0185ab478f663
Vladimir Eremin <personal@ermnvldmr.com>
2026-02-13 01:41:33 +0300

    ci: Fix lint in www && storybook build errors

--- services/www/src/widgets/Welcome.stories.tsx
+++ services/www/src/widgets/Welcome.stories.tsx
@@ -1,5 +1,6 @@
-import type { Meta, StoryObj } from '@storybook/react';
 import { Welcome } from './Welcome';
+
+import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof Welcome> = {
   title: 'Widgets/Welcome',
